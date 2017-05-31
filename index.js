23c90c3944329a6c40e88ee72da95e148cd3a16a
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-31 16:29:26 -0400

    ✨ scrap the whole year

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const Day = require('./lib/day');
+const { createRawDays, writeOutput } = require('./lib/utils');
 
-new Day(31, 12)
-  .then(info => console.log(info))
-  .catch(err => console.log(err));
+const RAW_DAYS = createRawDays();
+const OUTPUT_DIR = 'data.json';
+const DELAY = 50;
+
+const episodes = [];
+RAW_DAYS.forEach(({ day, month }, index) => {
+  setTimeout(() => episodes.push(new Day(day, month)), index * DELAY);
+});
+
+setTimeout(() => {
+  Promise.all(episodes)
+    .then(outputArray => writeOutput(OUTPUT_DIR, outputArray))
+    .catch(err => console.log(err));
+}, RAW_DAYS.length * DELAY);
