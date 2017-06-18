32b93defd52327a0edc24b3888dc8f0ccbf39afe
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-06-18 19:00:09 -0400

    ♻️ code cleanup

--- index.js
+++ index.js
@@ -5,26 +5,15 @@ function scraper(opts = {}) {
   const { delay, outputDir, logging } = opts;
   const msDelay = delay || 50;
 
-  const RAW_DAYS = createRawDays();
-  const episodes = [];
-
   if (logging) {
     console.log(`Scraper starting with a delay of ${msDelay}ms between requests.`);
   }
 
-  RAW_DAYS.forEach((date, index) => {
-    setTimeout(() => {
-      episodes.push(new Day(date.day, date.month, logging));
-    }, index * msDelay);
-  });
+  const dates = createRawDays()
+    .map((date, index) => new Day(date.day, date.month, delay*index, logging));
 
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      Promise.all(episodes)
-        .then(jsonArray => prepareOutput(res, rej, outputDir, jsonArray, logging))
-        .catch(rej);
-    }, RAW_DAYS.length * msDelay);
-  });
+  return Promise.all(dates)
+    .then(jsonArray => prepareOutput(outputDir, jsonArray, logging));
 }
 
 module.exports = scraper;
