6db826b0532c32d5be0e58756f45481a699a8871
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-06-18 12:20:04 -0400

    ♻️ accept options json for scraper rather than separate arguments

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const Day = require('./lib/day');
-const { createRawDays, writeOutput } = require('./lib/utils');
+const { createRawDays, prepareOutput } = require('./lib/utils');
 
-function scraper(outputDir = 'data.json', delay = 50) {
+function scraper(opts = {}) {
+  const { delay, outputDir, logging } = opts;
+  const msDelay = delay || 50;
+
   const RAW_DAYS = createRawDays();
   const episodes = [];
-  RAW_DAYS.forEach(({ day, month }, index) => {
-    setTimeout(() => episodes.push(new Day(day, month)), index * delay);
+
+  if (logging) {
+    console.log(`Scraper starting with a delay of ${msDelay}ms between requests.`);
+  }
+
+  RAW_DAYS.forEach((date, index) => {
+    setTimeout(() => {
+      episodes.push(new Day(date.day, date.month, logging));
+    }, index * msDelay);
   });
 
   return new Promise((res, rej) => {
     setTimeout(() => {
       Promise.all(episodes)
-        .then(outputArray => writeOutput(res, outputDir, outputArray))
+        .then(jsonArray => prepareOutput(res, rej, outputDir, jsonArray, logging))
         .catch(rej);
-    }, RAW_DAYS.length * delay);
+    }, RAW_DAYS.length * msDelay);
   });
 }
 
